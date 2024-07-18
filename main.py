9cdfc090e5d03dceeffa3371a03bdb63feb34d57
Kahngjoon Koh <kahngjoonk@gmail.com>
2024-07-19 03:29:37 +0900

    updated cors

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.route import router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://kahngjoonkoh.github.io/MediTaskAppointmentApp/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
